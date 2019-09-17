036a9d63ca2a39a4212f58742d95736b401edca0
Sergey Barsukov <s.barsukov@corp.mail.ru>
2019-09-17 23:57:40 +0300

    семинар №2 JS

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express')
 
 const app = express()
 
-const port = process.env.PORT || 8080
-
 app.use(express.static('public'))
 
+app.use((req, res) => {
+    res.sendFile(__dirname + '/public/404.html')
+});
+
+const port = process.env.PORT || 8080
 app.listen(port, function() {
     console.log('Running on http://localhost:' + port)
 });
